69e567f8daca872833bf069ff0b29514bd9b227d
graffhero <68558582+graffhero@users.noreply.github.com>
2020-08-07 15:40:59 -0500

    Update Comic_Database.py
    Final.

--- Comic_Database.py
+++ Comic_Database.py
@@ -2,29 +2,64 @@ import csv
 import re
 from typing import List
 
-f = open("Brett_Comic_Dump", "r")
-
+#f = open("Brett_Comic_Dump", "r")
+#comic_lines= csv.reader(f, delimiter= ";")
 
 def find_issue():
     issue= input("What issue(s) are you searchin' for? ")
-    for row in csv.reader(f, delimiter= ";"):
+    f = open("Brett_Comic_Dump", "r")
+    comic_lines = csv.reader(f, delimiter=";")
+    for row in comic_lines:
         if re.search(issue, str(row[0])):
             print(row[0], "issue", row[1])
-
+    f.close()
 
 def find_creator():
     author= input("Who are we searchin' for? ")
-    row: List[str]
-    for row in csv.reader(f, delimiter= ";"):
+    f = open("Brett_Comic_Dump", "r")
+    comic_lines = csv.reader(f, delimiter=";")
+    for row in comic_lines:
         if re.search(author, str(row[5])):
             print("The writer and artist(s) for", row[0] + " is: " + row[5])
+    f.close()
 
 def find_publish_date():
     released= input("What year are we searchin' for? ")
-    row: List[str]
-    for row in csv.reader(f, delimiter= ";"):
+    f = open("Brett_Comic_Dump", "r")
+    comic_lines = csv.reader(f, delimiter=";")
+    for row in comic_lines:
         if re.search(released, str(row[4])):
             print(row[0] + row[1], "was published in " + row[4])
+    f.close()
+
+def find_issue_number():
+    number = input("What issue number are we searchin' for? ")
+    f = open("Brett_Comic_Dump", "r")
+    comic_lines = csv.reader(f, delimiter=";")
+    for row in comic_lines:
+        if re.search(number, str(row[1])):
+            print("Issue", row[1], row[0])
+    f.close()
+
+def find_publisher():
+    printed_by = input("Which publisher are we searchin' for? Marvel, DC, Dark Horse, etc.")
+    f = open("Brett_Comic_Dump", "r")
+    comic_lines = csv.reader(f, delimiter=";")
+    for row in comic_lines:
+        if re.search(printed_by, str(row[3])):
+            print("Publisher", row[3], row[0])
+    f.close()
+
+def find_extended_list():
+    title_all_info = input("Which issues or series are we searchin' for?")
+    f = open("Brett_Comic_Dump", "r")
+    comic_lines = csv.reader(f, delimiter=";")
+    for row in comic_lines:
+        if re.search(title_all_info, str(row[0])):
+            print(row[0], "issue number", row[1], "issue title:", row[2], "printed by:", row[3], "on", row[4],
+                  "and writen and drawn by", row[5])
+    f.close()
+
 
 
 def main():
@@ -33,13 +68,23 @@ def main():
         ("""Welcome to Brett\'s Nerd Collection! What would you like to see?
         Press 1 to search by Issue Name.
         Press 2 to search by Author/Artist(s).
-        Press 3 to search by Release Date""")
+        Press 3 to search by Release Date.
+        Press 4 to search by Issue Number.
+        Press 5 to search by Publisher.
+        Press 6 to search by Issue Name and ALL Information.""")
+
         if choice == "1":
             find_issue()
         elif choice == "2":
             find_creator()
         elif choice == "3":
             find_publish_date()
+        elif choice == "4":
+            find_issue_number()
+        elif choice == "5":
+            find_publisher()
+        elif choice == "6":
+            find_extended_list()
         else:
             print("I\'m sorry fellow nerd, please try again!")
 
