a0925378455da9e3df0d2f9a5cfdd790842daee2
graffhero <68558582+graffhero@users.noreply.github.com>
2020-08-07 13:06:59 -0500

    Update Comic_Database.py
    Progress with idiot proof.

--- Comic_Database.py
+++ Comic_Database.py
@@ -6,10 +6,10 @@ f = open("Brett_Comic_Dump", "r")
 
 
 def find_issue():
-    issue= input("What we searchin' for? ")
+    issue= input("What issue(s) are you searchin' for? ")
     for row in csv.reader(f, delimiter= ";"):
         if re.search(issue, str(row[0])):
-            print(row[0])
+            print(row[0], "issue", row[1])
 
 
 def find_creator():
@@ -17,14 +17,42 @@ def find_creator():
     row: List[str]
     for row in csv.reader(f, delimiter= ";"):
         if re.search(author, str(row[5])):
-            print(row[0] + " was written by " + row[5])
+            print("The writer and artist(s) for", row[0] + " is: " + row[5])
+
+def find_publish_date():
+    released= input("What year are we searchin' for? ")
+    row: List[str]
+    for row in csv.reader(f, delimiter= ";"):
+        if re.search(released, str(row[4])):
+            print(row[0] + row[1], "was published in " + row[4])
 
 
 def main():
-    choice= input("Press 1 to search by issue name. "
-                  "Press 2 to search by author.")
-    if choice == "1":
-        find_issue()
-    elif choice == "2":
-        find_creator()
+    while True:
+        choice= input\
+        ("""Welcome to Brett\'s Nerd Collection! What would you like to see?
+        Press 1 to search by Issue Name.
+        Press 2 to search by Author/Artist(s).
+        Press 3 to search by Release Date""")
+        if choice == "1":
+            find_issue()
+        elif choice == "2":
+            find_creator()
+        elif choice == "3":
+            find_publish_date()
+        else:
+            print("I\'m sorry fellow nerd, please try again!")
+
+        answer = input("Would you like to try again? Y or N?")
+
+        if answer not in ("y", "n"):
+            print("Wrong again, geez! Goodbye!")
+            break
+
+        if answer == "y":
+            continue
+        else:
+            input("\nExcelsior! Press ENTER to exit")
+            break
+
 main()
