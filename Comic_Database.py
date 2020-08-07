3836819cc3a135a37c9e912e24a85cbaeaa6d692
graffhero <68558582+graffhero@users.noreply.github.com>
2020-08-07 09:23:33 -0500

    Create Comic_Database.py

---
+++ Comic_Database.py
@@ -0,0 +1,30 @@
+import csv
+import re
+from typing import List
+
+f = open("Brett_Comic_Dump", "r")
+
+
+def find_issue():
+    issue= input("What we searchin' for? ")
+    for row in csv.reader(f, delimiter= ","):
+        if re.search(issue, str(row[0])):
+            print(row[0])
+
+
+def find_creator():
+    author= input("Who are we searchin' for? ")
+    row: List[str]
+    for row in csv.reader(f, delimiter= ","):
+        if re.search(author, str(row[5])):
+            print(row[0] + " was written by " + row[5])
+
+
+def main():
+    choice= input("Press 1 to search by issue name. "
+                  "Press 2 to search by author.")
+    if choice == "1":
+        find_issue()
+    elif choice == "2":
+        find_creator()
+main()
